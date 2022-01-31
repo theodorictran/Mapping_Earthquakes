eae637b6724af1476ad4daea3fea645ff2f529d2
Theo Tran <theodorictran@gmail.com>
2022-01-30 18:46:41 -0600

    added popup feature

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -34,12 +34,21 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/theodorictran/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+// Accessing the Toronto GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/theodorictran/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
+  
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  color: "#ffffa1",
+  weight: 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: " + 
+    feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
 });
